8ed69cad583ae3e3b20779fa7ab3988b84751ecb
elisayadarola <{58332238}+elisayadarola@users.noreply.github.com>
2023-03-25 12:30:09 -0400

    tr to fix the count function for the clicks

--- vinted_academy.py
+++ vinted_academy.py
@@ -14,8 +14,12 @@ def read_database(data):
             with open(os.path.join(data, file), newline='') as csvfile:
                 # store the csvfile data in the csv reader object. dictreader embedded function return an ordered dictionary for each row.
                 reader = csv.DictReader(csvfile)
-                # using the extend method to add the rows from each CSV file to the csv_file. If I used append instead of extend, because I want to add all the dictionaries to the list individually and not the entire reader object as one element.
+                # using the extend method to add the rows from each CSV file to the csv_file.  I used extend instead of append, because I want to add all the dictionaries to the list individually and not the entire reader object as one element.
                 csv_files.extend(reader)
+                for row in reader:
+                    clicks_value = row["click_target"]
+                    print(clicks_value)
+
     # I get the list with the data already ordered by row. 
     return csv_files
 
@@ -26,22 +30,28 @@ all_user_data = clicks_csv + users_csv
 # print(all_user_data)
 
 # here starts the mapping of the info. I will need two parameters for the map function, so the first will be the function obtaining two rows containig the date and the clicks info
-def get_columns(row):
-   try:
-    return{"Date":row["date"], "Clicks":row["click_target"]}
-   except ValueError:
-    return{"Date":row["date"], "Clicks":0}
+# def get_columns(row):
+# #    try:
+#     return{"Date":row["date"], "Clicks":row["click_target"]}
+   
+# #    except ValueError:
+# #     return{"Date":row["date"], "Clicks":0}
  
 
 
-# so here I couldn't use 'clicks_csv' for mapping because the parameter didn't accept a list, so I had to input the specific directory path. 
-rows = read_database('data/clicks')
-get_rows = map(get_columns, rows)
+# # And then i map the rows. So here I couldn't use 'clicks_csv' for mapping because the parameter didn't accept a dictionary list, so I had to input the specific directory path. 
+# rows = read_database('data/clicks')
+# get_rows = map(get_columns, rows)
 
+# for row in get_rows:
+#     print(row)
+# # now I need to count the number of clicks
+# number_of_clicks = 0
+# for row in clicks_csv:
+#     if row["Clicks"] == "click_target":
+#         number_of_clicks += 1
+# # number_of_clicks= sum(row["Clicks"].count(char) for row in get_rows for char in row["Clicks"])
+# print(f"Total clicks: {number_of_clicks} ")
 
-# now I need to count the number of clicks
-
-
-
-
+# # print(int(clicks_csv.count("click_target")))
 
