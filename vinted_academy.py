1a5bf4c51b80e7c2b5be2b8e42bcdeb6baca206f
elisayadarola <{58332238}+elisayadarola@users.noreply.github.com>
2023-03-25 14:05:47 -0400

    tur dict into a list

--- vinted_academy.py
+++ vinted_academy.py
@@ -15,13 +15,11 @@ def read_database(data):
                 # store the csvfile data in the csv reader object. dictreader embedded function return an ordered dictionary for each row.
                 reader = csv.DictReader(csvfile)
                 # using the extend method to add the rows from each CSV file to the csv_file.  I used extend instead of append, because I want to add all the dictionaries to the list individually and not the entire reader object as one element.
-                csv_files.extend(reader)
-                for row in reader:
-                    clicks_value = row["click_target"]
-                    print(clicks_value)
-
+                csv_files.extend(reader)        
+               
     # I get the list with the data already ordered by row. 
-    return csv_files
+    return list(csv_files)
+
 
 clicks_csv = read_database('data/clicks')
 users_csv = read_database('data/users')
@@ -29,29 +27,36 @@ all_user_data = clicks_csv + users_csv
 # print(f"the count is: {clicks_csv.count(read_database('data/clicks'))}")
 # print(all_user_data)
 
-# here starts the mapping of the info. I will need two parameters for the map function, so the first will be the function obtaining two rows containig the date and the clicks info
-# def get_columns(row):
-# #    try:
-#     return{"Date":row["date"], "Clicks":row["click_target"]}
-   
-# #    except ValueError:
-# #     return{"Date":row["date"], "Clicks":0}
- 
+# here starts the mapping of the info. I will need two parameters for the map function, so the first will be the function obtaining two rows containing the date and the clicks info
+def get_columns(row):
+   try:
+    return{"Date":row["date"], "Clicks":row["click_target"]}
+   except ValueError:
+    return{"Date":row["date"], "Clicks":0}
+
+# def clicks_per_date(rows):
+#     click_count = {}
+#     for row in rows:
+#         date = row["Date"]
+#         clicks = int(row["Clicks"])
+#         if date in click_count:
+#             click_count[date] += clicks
+#         else:
+#             click_count[date] = clicks
+#     return [{"Date": date, "count": count} for date, count in click_count.items()]
+
+# print(clicks_per_date)
+
+# And then i map the rows. So here I couldn't use 'clicks_csv' for mapping because the parameter didn't accept a dictionary list, so I had to input the specific directory path. 
+rows = read_database('data/clicks')
+get_rows = map(get_columns, rows)
+#so for a dictionary we use len() to instead of count
+for row in get_rows:
+            clicks_value = row["Clicks"]
+            print(row, len(clicks_value))
+          
 
 
-# # And then i map the rows. So here I couldn't use 'clicks_csv' for mapping because the parameter didn't accept a dictionary list, so I had to input the specific directory path. 
-# rows = read_database('data/clicks')
-# get_rows = map(get_columns, rows)
+# now I need to count the number of clicks
 
-# for row in get_rows:
-#     print(row)
-# # now I need to count the number of clicks
-# number_of_clicks = 0
-# for row in clicks_csv:
-#     if row["Clicks"] == "click_target":
-#         number_of_clicks += 1
-# # number_of_clicks= sum(row["Clicks"].count(char) for row in get_rows for char in row["Clicks"])
-# print(f"Total clicks: {number_of_clicks} ")
-
-# # print(int(clicks_csv.count("click_target")))
 
