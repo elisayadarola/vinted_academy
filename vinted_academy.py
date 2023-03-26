e3190850acba3633d7570816a5e3e080d32099ae
elisayadarola <{58332238}+elisayadarola@users.noreply.github.com>
2023-03-26 16:29:33 -0400

    changing strategy to get the count

--- vinted_academy.py
+++ vinted_academy.py
@@ -1,8 +1,9 @@
-
 # I import the os so I can loop through it
 import os
 # I need to read the data in the CSV files first, so I import CSV for that matter. It's important that the database files are placed in the same folder as the code. 
 import csv
+#I import the reduce function by VS Code suggestions after trying to use reduce without defining it
+from functools import reduce
 
 def read_database(data):
     # I pass it an empty list that will receive all of the .csv data found in the loop.
@@ -18,9 +19,9 @@ def read_database(data):
                 csv_files.extend(reader)        
                
     # I get the list with the data already ordered by row. 
-    return list(csv_files)
+    return csv_files
 
-
+#attributing the data to a variable to be called or used later if necessary. 
 clicks_csv = read_database('data/clicks')
 users_csv = read_database('data/users')
 all_user_data = clicks_csv + users_csv
@@ -28,11 +29,24 @@ all_user_data = clicks_csv + users_csv
 # print(all_user_data)
 
 # here starts the mapping of the info. I will need two parameters for the map function, so the first will be the function obtaining two rows containing the date and the clicks info
+# def get_columns(row):
+#    try:
+#     return{"Date":row["date"], "Clicks":row["click_target"]}
+#    except ValueError:
+#     return{"Date":row["date"], "Clicks":0}
+
 def get_columns(row):
-   try:
-    return{"Date":row["date"], "Clicks":row["click_target"]}
-   except ValueError:
-    return{"Date":row["date"], "Clicks":0}
+   
+    if row["click_target"] != "":
+        return 1
+    else:
+        return 0
+   
+
+click_count = map(get_columns, clicks_csv)
+total_count = reduce(lambda a,b : a + b, click_count)
+
+print(total_count)
 
 # def clicks_per_date(rows):
 #     click_count = {}
@@ -47,16 +61,47 @@ def get_columns(row):
 
 # print(clicks_per_date)
 
-# And then i map the rows. So here I couldn't use 'clicks_csv' for mapping because the parameter didn't accept a dictionary list, so I had to input the specific directory path. 
-rows = read_database('data/clicks')
-get_rows = map(get_columns, rows)
+# And then i map the rows. So here I couldn't use 'clicks_csv' for mapping because the parameter didn't accept a dictionary, so I had to input the specific directory path. 
+
+get_rows = map(get_columns, clicks_csv)
 #so for a dictionary we use len() to instead of count
-for row in get_rows:
-            clicks_value = row["Clicks"]
-            print(row, len(clicks_value))
+# for row in get_rows:
+#             clicks_value = row["Clicks"]
+#             print(row, len(clicks_value))
           
 
 
 # now I need to count the number of clicks
+# import csv
+# import os
 
+# # Function to read data from CSV files into a list of dictionaries
+# def read_database(data):
+#     csv_files = []
+#     for file in os.listdir(data):
+#         if file.endswith('.csv'):
+#             with open(os.path.join(data, file), newline='') as csvfile:
+#                 reader = csv.DictReader(csvfile)
+#                 csv_files.extend(reader)        
+#     return list(csv_files)
 
+# # Read clicks data into a list of dictionaries
+# clicks = read_database('data/clicks')
+
+# # Extract "date" and "click_target" columns and count clicks for each date
+# click_counts = {}
+# for row in clicks:
+#     date = row["date"]
+#     clicks = int(row["click_target"])
+#     if date in click_counts:
+#         click_counts[date] += clicks
+#     else:
+#         click_counts[date] = clicks
+
+# # Write results to CSV file
+# with open('data/total_clicks.csv', 'w', newline='') as csvfile:
+#     fieldnames = ['date', 'count']
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#     writer.writeheader()
+#     for date, count in click_counts.items():
+#         writer.writerow({'date': date, 'count': count})
